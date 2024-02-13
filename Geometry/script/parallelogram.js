94cbba66f73f142113d9d58563d0b60e2514bab7
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-02-13 23:26:33 +0600

    Created A Reusable Function For Similar Task

--- Geometry/script/parallelogram.js
+++ Geometry/script/parallelogram.js
@@ -24,16 +24,16 @@ function calculateParallelogramArea() {
 }
 */
 
-function calculateParallelogramArea() {
-    // const baseInput = document.getElementById('parallelogram-base');
-    // const baseText = baseInput.value;
-    // const base = parseFloat(baseText);
-    // console.log(base);
+// function calculateParallelogramArea() {
+// const baseInput = document.getElementById('parallelogram-base');
+// const baseText = baseInput.value;
+// const base = parseFloat(baseText);
+// console.log(base);
 
-    const base = getParallelogramBase();
-    console.log(base)
+// const base = getParallelogramBase();
+// console.log(base)
 
-}
+// }
 
 // function getParallelogramBase() {
 //     const baseInput = document.getElementById('parallelogram-base');
@@ -42,3 +42,33 @@ function calculateParallelogramArea() {
 //     return base;
 
 // }
+
+
+
+function calculateParallelogramArea() {
+    const base = getInputValueById('parallelogram-base');
+    // console.log('base value', base);
+
+    const height = getInputValueById('parallelogram-height');
+    // console.log('HEight value', height);
+
+    const area = base * height;
+    // console.log('area', area);
+
+    setInnerTextById('parallelogram-area', area)
+
+}
+
+function getInputValueById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputValueText = inputField.value;
+    const inputValue = parseFloat(inputValueText);
+    // console.log(inputValue)
+    return inputValue;
+}
+
+function setInnerTextById(elementId, area) {
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+
+}
