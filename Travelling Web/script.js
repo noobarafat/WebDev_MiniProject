75ecaa96ca600e49a3f88638cc573d0fd7cb725b
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-02-16 21:56:35 +0600

    onnected cart count with add card button

--- Travelling Web/script.js
+++ Travelling Web/script.js
@@ -1,1 +1,8 @@
- 
+const allBtn = document.getElementsByClassName("add-btn");
+let count = 0;
+for (const btn of allBtn) {
+    btn.addEventListener("click", function (e) {
+        count += 1;
+        document.getElementById("cart-count").innerText = count;
+    });
+}
